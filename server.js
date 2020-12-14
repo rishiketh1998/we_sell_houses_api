705a713fa98679aa09821ad067e4efd6ee4bb3c9
rishi <prabhak8@uni.coventry.ac.uk>
2020-12-14 08:16:21 +0000

    updated port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./index.js')
 /**
  * @description: starts the server on "process.env.PORT" port
  */
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`server running on ${process.env.PORT}`)
 })
