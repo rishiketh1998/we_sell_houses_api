b7d399649e009f09ce7aaf0354d96d7135cde59b
rishi <prabhak8@uni.coventry.ac.uk>
2020-12-14 12:06:43 +0000

    updated route name

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
 app.use(cookieParser())
-app.use('/api/v1/users', users)
-app.use('/api/v1/login', login)
-app.use('/api/v1/logout', logout)
-app.use('/api/v1/properties', properties)
-app.use('/api/v1/messages', messages)
-app.use('/api/v1/areaProperties', areaProperties)
+app.use('/v1/users', users)
+app.use('/v1/login', login)
+app.use('/v1/logout', logout)
+app.use('/v1/properties', properties)
+app.use('/v1/messages', messages)
+app.use('/v1/areaProperties', areaProperties)
 
 app.get('/', (req,res) => {
     res.send('We Sell Houses Backend')
